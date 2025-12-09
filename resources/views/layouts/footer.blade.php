82531fae31be3e044be9b83f06e53ec0463190a8
khanumnawraattar@gmail.com <khanumnawraattar@gmail.com>
2025-12-09 11:14:19 +0700

    database_1

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Famaindo 8kreasi Audia</span>
+                        <span>Copyright &copy; PT Famaindo Delapan Kreasi</span>
                     </div>
                 </div>
             </footer>
